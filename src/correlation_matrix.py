e88d423a398a89e68ba7c8e0b2903265a0217ec1
gabrielfruet <gabrielfruet538@gmail.com>
2025-02-06 22:53:35 -0300

    fix: cov is other method

--- src/correlation_matrix.py
+++ src/correlation_matrix.py
@@ -1,7 +1,9 @@
 import jax.numpy as jnp
 import jax
 from jax import vmap
+from functools import partial
 
+@partial(jax.jit, static_argnums=(2,3))
 def correlate(x,y=None, p=None, cov=False):
     if y is None:
         y = x
@@ -21,22 +23,38 @@ def correlate(x,y=None, p=None, cov=False):
     assert p <= n, "p must be less than or equal to the number of columns in X\
     and Y"
 
-    X = lagged_matrix(x - x.mean(), p)
-    Y = lagged_matrix(y - y.mean(), p)
-
     if cov:
-        return (Y.T @ X)/(n-p+1)
-    return (Y.T @ X)/n
+        X = cov_lagged_matrix(x, p)
+        Y = cov_lagged_matrix(y, p)
 
-def lagged_matrix(X,p=3):
-    n = len(X)
-    M = jnp.zeros((p,n+p-1))
+        return (Y.T @ X)/(n-p+1)
+    else:
+        X = lagged_matrix(x, p)
+        Y = lagged_matrix(y, p)
+
+        return (Y.T @ X)/n
+
+
+@partial(jax.jit, static_argnums=(1,))
+def lagged_matrix(X, p=3):
+    M = []
 
     for i in range(p):
-        M = M.at[i, i:n+i].set(X)
+        x = jnp.pad(X, (i, p - 1 - i), mode='constant', constant_values=0)
+        M.append(x)
 
-    return M.T
+    return jnp.array(M).T
 
+
+@partial(jax.jit, static_argnums=(1,))
+def cov_lagged_matrix(X, p=3):
+    n = X.shape[0]
+    M = []
+    for i in range(p):
+        x = X[p-i-1: n-i-1][::-1]
+        M.append(x)
+
+    return jnp.array(M).T
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -51,7 +69,7 @@ if __name__ == '__main__':
 
     uncorrelated_signal = jax.random.normal(seed, (n_samples,))
 
-    c1 = correlate(uncorrelated_signal, correlated_signal, 10)
+    c1 = correlate(uncorrelated_signal, correlated_signal, 10, cov=True)
     plt.imshow(c1)
     plt.show()
 
